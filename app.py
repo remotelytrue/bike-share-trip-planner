0204eb104ba5d200d749f9cab0a956e014fb9003
Phillip Steindel <pstein@bu.edu>
2016-05-27 09:28:51 -0400

    leaflet site

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect
 from work import get_current_station_data, get_current_info
 from datetime import datetime
 import pandas
+import json
 
 app = Flask(__name__)
 app.have_locs = False
@@ -35,10 +36,33 @@ def station_map():
   if not app.have_locs:
     return redirect('/index')
   else:
-    start_info, end_info, (script, div) = get_current_station_data.get_map(app.station_info, 
-                                                   app.features, app.start_point, app.end_point)
-    return render_template('station_map.html', start_info = start_info,
-                           end_info = end_info, script=script, div=div)
+    #start_info, end_info, (script, div) = get_current_station_data.get_map(app.station_info, 
+    #                                               app.features, app.start_point, app.end_point)
+    return render_template('station_map.html')
+
+@app.route('/start_loc.json')
+def start_loc():
+  lati, longi = get_current_station_data.get_google_info(app.start_point+', New York, NY')
+  app.start_pos = [lati, longi]
+  return json.dumps(app.start_pos)
+
+@app.route('/end_loc.json')
+def end_loc():
+  lati, longi = get_current_station_data.get_google_info(app.end_point+', New York, NY')
+  app.end_pos = [lati, longi]
+  return json.dumps(app.end_pos)
+
+@app.route('/start_predictions.json')
+def start_predictions():
+  start_json = get_current_station_data.get_start_stations(app.start_pos, app.features, app.station_info)
+  #will be sentence, geojson of points
+  return start_json
+
+@app.route('/end_predictions.json')
+def end_predictions():
+  end_json = get_current_station_data.get_end_stations(app.end_pos, app.features, app.station_info)
+  #will be sentence, gejson of points
+  return end_json
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -53,4 +77,4 @@ def error():
   return render_template('error.html')
 
 if __name__ == '__main__':
-  app.run(port=3357) 
+  app.run(port=3357, debug=True) 
--- work/get_current_station_data.py
+++ work/get_current_station_data.py
@@ -3,12 +3,8 @@ import pandas as pd
 import os.path
 import dill
 import bz2
-from bokeh.io import gridplot
-from bokeh.models import (
-  GMapPlot, GMapOptions, ColumnDataSource, Circle, DataRange1d, PanTool, 
-  WheelZoomTool, BoxSelectTool, Text
-)
-from bokeh.embed import components
+import json
+import numpy as np
 
 def get_google_info(search_term):
   url = 'https://maps.googleapis.com/maps/api/geocode/json'
@@ -18,6 +14,7 @@ def get_google_info(search_term):
   location = results[0]['geometry']['location']
   return location['lat'], location['lng']
 
+
 def predict_checkouts(id, n_bikes, total_docks, features):
   if not (os.path.exists('estimators/checkout_estimator_high_'+str(id)+'.dill.bz2') and 
           os.path.exists('estimators/checkin_estimator_mid_'+str(id)+'.dill.bz2')):
@@ -25,8 +22,9 @@ def predict_checkouts(id, n_bikes, total_docks, features):
   checkout_est = dill.load(bz2.BZ2File('estimators/checkout_estimator_high_'+str(id)+'.dill.bz2', 'r'))
   checkin_est = dill.load(bz2.BZ2File('estimators/checkin_estimator_mid_'+str(id)+'.dill.bz2', 'r'))
   flux = checkin_est.predict(features)/15 - checkout_est.predict(features)/15
-  bikes = n_bikes+flux.cumsum()
+  bikes = np.clip(n_bikes+flux.cumsum(), 0, total_docks)
   return int(bikes[4]), int(bikes[9]), int(bikes[14]), int(bikes[19])
+
 
 def predict_checkins(id, n_docks, total_docks, features):
   if not (os.path.exists('estimators/checkin_estimator_high_'+str(id)+'.dill.bz2') and 
@@ -35,8 +33,9 @@ def predict_checkins(id, n_docks, total_docks, features):
   checkin_est = dill.load(bz2.BZ2File('estimators/checkin_estimator_high_'+str(id)+'.dill.bz2', 'r'))
   checkout_est = dill.load(bz2.BZ2File('estimators/checkout_estimator_mid_'+str(id)+'.dill.bz2', 'r'))
   flux = checkout_est.predict(features)/15 - checkin_est.predict(features)/15
-  docks = n_docks+flux.cumsum()
+  docks = np.clip(n_docks+flux.cumsum(), 0, total_docks)
   return int(docks[29]), int(docks[44]), int(docks[59])
+
 
 def pick_checkout(sorted_stations):
   picked_flag = 0
@@ -53,6 +52,7 @@ def pick_checkout(sorted_stations):
       break
   return sorted_stations
 
+
 def pick_checkin(sorted_stations):
   picked_flag = 0
   for time in ['60 min', '45 min', '30 min', '0 min']:
@@ -68,9 +68,38 @@ def pick_checkin(sorted_stations):
       break
   return sorted_stations
 
-def get_points(start_lat, start_lon, end_lat, end_lon, station_info, features):
+
+def bike_string(n_bikes):
+  if n_bikes == 1:
+    return '1 bike'
+  else:
+    return '{} bikes'.format(n_bikes)
+
+
+def generate_start_popup(row):
+  return """<p>{}<br>Current: {}<br>5 minutes: {}<br>
+         10 minutes: {}<br>15 minutes: {}<br>20 minutes: {}</p>"""\
+         .format(row["station_name"], bike_string(row['0 min']), bike_string(row['5 min']),
+                 bike_string(row['10 min']), bike_string(row['15 min']),
+                 bike_string(row['20 min']))
+
+
+def dock_string(n_docks):
+  if n_docks == 1:
+    return '1 dock'
+  else:
+    return '{} docks'.format(n_docks)
+
+
+def generate_end_popup(row):
+  return "<p>{}<br>Current: {}<br>30 minutes: {}<br>45 minutes: {}<br>60 minutes: {}</p>"\
+         .format(row["station_name"], dock_string(row['0 min']), dock_string(row['30 min']),
+                 dock_string(row['45 min']), dock_string(row['60 min']))
+
+
+def get_start_stations(start_pos, features, station_info):
   to_plot_start = []
-  to_plot_end = []
+  [start_lat, start_lon] = start_pos
   for i, row in station_info.iterrows():
     if row['statusValue'] == 'In Service':
       if abs(row['latitude']-start_lat) < 0.005 and abs(row['longitude']-start_lon) < 0.005:
@@ -79,133 +108,118 @@ def get_points(start_lat, start_lon, end_lat, end_lon, station_info, features):
         new_row['longitude'] = row['longitude']
         new_row['station_name'] = row['stationName']
         new_row['0 min'] = row['availableBikes']
-        if row['availableBikes'] == 1:
-          new_row['bike_string'] = '1 bike'
-        else:
-          new_row['bike_string'] = str(row['availableBikes'])+' bikes'
         new_row['5 min'], new_row['10 min'], new_row['15 min'], new_row['20 min'] = predict_checkouts(row['id'], row['availableBikes'],
-                                                                 row['totalDocks'], features)
+                                                                                    row['totalDocks'], features)
         to_plot_start.append(new_row)
-      
+  
+  to_plot_start_sorted = sorted(to_plot_start, 
+                                key=lambda x: \
+                                (x['latitude']-start_lat)**2 + (x['longitude']-start_lon)**2)
+  to_plot_start_sorted_picked = pick_checkout(to_plot_start_sorted)
+  
+  for_leaflet = []
+  start_info = ""
+  for row in to_plot_start_sorted_picked:
+    geojson_feature = { "type": "Feature",
+                        "properties": {
+                            "name": row['station_name'],
+                            "popupContent": generate_start_popup(row)
+                        },
+                        "geometry": {
+                            "type": "Point",
+                            "coordinates": [row['longitude'], row['latitude']]
+                        }
+                      }
+    
+    geojson_style = { "radius": 8,
+                      "fillColor": row['color'],
+                      "color": "#000",
+                      "weight": 1,
+                      "opacity": 1,
+                      "fillOpacity": 0.8
+                    }
+
+    if row['color'] == 'black':
+      start_info = """Recommended start station: {}.  
+                      There are currently {} bikes there. 
+                      In 5 minutes, I predict there will be {} 
+                      bikes; 10 minutes, {} bikes;
+                      15 minutes, {} bikes; and in 20 minutes, 
+                      {} bikes"""\
+                      .format(row['station_name'], row['0 min'], 
+                              row['5 min'], row['10 min'],
+                              row['15 min'], row['20 min'])
+
+    for_leaflet.append([geojson_feature, geojson_style])
+
+  if not start_info:
+    if not to_plot_start_sorted_picked:
+      start_info = 'No stations near your start point!'
+    else:
+      start_info = 'No stations near you have bikes!'
+
+  return json.dumps([start_info, for_leaflet])
+
+
+def get_end_stations(end_pos, features, station_info):
+  to_plot_end = []
+  [end_lat, end_lon] = end_pos
+  for i, row in station_info.iterrows():
+    if row['statusValue'] == 'In Service':
       if abs(row['latitude']-end_lat) < 0.005 and abs(row['longitude']-end_lon) < 0.005:
         new_row = {}
         new_row['latitude'] = row['latitude']
         new_row['longitude'] = row['longitude']
         new_row['station_name'] = row['stationName']
         new_row['0 min'] = row['availableDocks']
-        if row['availableDocks'] == 1:
-          new_row['dock_string'] = '1 dock'
-        else:
-          new_row['dock_string'] = str(row['availableDocks'])+' docks'
         new_row['30 min'], new_row['45 min'], new_row['60 min'] = predict_checkins(row['id'], row['availableDocks'],
-                                             row['totalDocks'], features)
+                                                                  row['totalDocks'], features)
         to_plot_end.append(new_row)
-  
-  to_plot_start_sorted = sorted(to_plot_start, 
-                                key=lambda x: (x['latitude']-start_lat)**2 + (x['longitude']-start_lon)**2)
   to_plot_end_sorted = sorted(to_plot_end, 
-                                key=lambda x: (x['latitude']-end_lat)**2 + (x['longitude']-end_lon)**2)
-  return to_plot_start_sorted, to_plot_end_sorted
-
-def get_map(station_info, features, start_point, end_point, city = 'New York, NY'):
-  start_lat, start_lon = get_google_info(start_point+', '+city)
-  end_lat, end_lon = get_google_info(end_point+', '+city)
-
-  map1_options = GMapOptions(lat=start_lat, lng=start_lon, map_type="roadmap", zoom=16)
-  plot1 = GMapPlot(
-      x_range=DataRange1d(), y_range=DataRange1d(), 
-      map_options=map1_options, 
-      title="Start stations with current bikes"
-  )
-  plot1.title_text_font_size = '16pt'
-  plot1.plot_width = 500
-  plot1.plot_height = 500
-
-  map2_options = GMapOptions(lat=end_lat, lng=end_lon, map_type="roadmap", zoom=16)
-  plot2 = GMapPlot(
-      x_range=DataRange1d(), y_range=DataRange1d(), 
-      map_options=map2_options, 
-      title="End stations with current docks"
-  )
-  plot2.title_text_font_size = '16pt'
-  plot2.plot_width = 500
-  plot2.plot_height = 500
-
-  to_plot_start_sorted, to_plot_end_sorted = get_points(start_lat, start_lon, end_lat, end_lon, station_info, features)
-  to_plot_start_sorted_picked = pick_checkout(to_plot_start_sorted)
+                                key=lambda x: \
+                                (x['latitude']-end_lat)**2 + (x['longitude']-end_lon)**2)
   to_plot_end_sorted_picked = pick_checkin(to_plot_end_sorted)
 
-  for row in to_plot_start_sorted_picked:    
-    source = ColumnDataSource(
-           data=dict(
-            lat=[row['latitude']],
-            lon=[row['longitude']],
-            lon_txt = [row['longitude']+0.0003],
-            bike_text=[str(row['0 min'])],
-            )
-           )
-    bike_text_glyph = Text(x="lon_txt", y="lat", text='bike_text', 
-                           text_baseline='middle', text_align='left', text_font_style='bold')
-    circle = Circle(x="lon", y="lat", size=20, fill_color=row['color'], fill_alpha=1)
-    plot1.add_glyph(source, circle)
-    plot1.add_glyph(source, bike_text_glyph)
+  for_leaflet = []
+  end_info = ""
+  for row in to_plot_end_sorted_picked:
+    geojson_feature = { "type": "Feature",
+                        "properties": {
+                            "name": row['station_name'],
+                            "popupContent": generate_end_popup(row)
+                        },
+                        "geometry": {
+                            "type": "Point",
+                            "coordinates": [row['longitude'], row['latitude']]
+                        }
+                      }
+    
+    geojson_style = { "radius": 8,
+                      "fillColor": row['color'],
+                      "color": "#000",
+                      "weight": 1,
+                      "opacity": 1,
+                      "fillOpacity": 0.8
+                    }
+
     if row['color'] == 'black':
-      start_info = {}
-      start_info['name'] = row['station_name']
-      start_info['0 min'] = str(row['0 min'])
-      start_info['5 min'] = str(row['5 min'])
-      start_info['10 min'] = str(row['10 min'])
-      start_info['15 min'] = str(row['15 min'])
-      start_info['20 min'] = str(row['20 min'])
+      end_info = """Recommended end station: {}. 
+                    There are currently {} docks there.
+                    In 30 minutes, I predict there will be {} 
+                    docks; 45 minutes, {} docks;  
+                    and in 60 minutes, {} docks"""\
+                    .format(row['station_name'], row['0 min'], row['30 min'],
+                            row['45 min'], row['60 min'])
 
-  for row in to_plot_end_sorted_picked:    
-    source = ColumnDataSource(
-           data=dict(
-            lat=[row['latitude']],
-            lon=[row['longitude']],
-            lon_txt = [row['longitude']+0.0003],
-            dock_text=[str(row['0 min'])],
-            )
-           )
-    dock_text_glyph = Text(x="lon_txt", y="lat", text='dock_text', 
-                           text_baseline='middle', text_align='left', text_font_style='bold')
-    circle = Circle(x="lon", y="lat", size=20, fill_color=row['color'], fill_alpha=1)
-    plot2.add_glyph(source, circle)
-    plot2.add_glyph(source, dock_text_glyph)
-    if row['color'] == 'black':
-      end_info = {}
-      end_info['name'] = row['station_name']
-      end_info['0 min'] = str(row['0 min'])
-      end_info['30 min'] = str(row['30 min'])
-      end_info['45 min'] = str(row['45 min'])
-      end_info['60 min'] = str(row['60 min'])
 
-  plot1.add_tools(PanTool(), WheelZoomTool())
-  plot2.add_tools(PanTool(), WheelZoomTool())
+    for_leaflet.append([geojson_feature, geojson_style])
 
-  p = gridplot([[plot1, plot2]])
-  
-  if not start_info:
-    start_info = {}
-    if not to_plot_start_sorted_picked:
-      start_info['name'] = 'No stations near your start point!'
-    else:
-      start_info['name'] = 'No stations near you have bikes!'
-    start_info['0 min'] = 'N/A'
-    start_info['5 min'] = 'N/A'
-    start_info['10 min'] = 'N/A'
-    start_info['15 min'] = 'N/A'
   if not end_info:
-    end_info = {}
     if not to_plot_end_sorted_picked:
-      end_info['name'] = 'No stations near your end point!'
+      end_info = 'No stations near your end point!'
     else:
-      end_info['name'] = 'No stations have bikes at your end point!'
-    end_info['30 min'] = 'N/A'
-    end_info['45 min'] = 'N/A'
-    end_info['60 min'] = 'N/A'
-  
-  return start_info, end_info, components(p)
+      end_info = 'No stations have bikes at your end point!'
 
+  return json.dumps([end_info, for_leaflet])
 
 
